603df757cb5de8468dad74c817ef455ea0fa8f0d
jason.cai <jason.cai@veoneer.com>
2023-04-20 23:46:04 +0800

    V1.1.8.4 U3

---
+++ WinPMS/Debug/FrmDebugConfigTransfer.cs
@@ -0,0 +1,51 @@
+﻿using PMS.COMMON.Helper;
+using System;
+using System.Windows.Forms;
+
+namespace WinPMS.Debug
+{
+    public partial class FrmDebugConfigTransfer : Form
+    {
+        public FrmDebugConfigTransfer()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+
+        #region 事件
+
+
+
+
+
+
+        private void tsbtnClose_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.Yes == MsgBoxHelper.MsgBoxConfirm("Close" + " ？", "Info", 2))
+            {
+                this.CloseForm();
+            }
+        }
+
+
+
+
+
+
+        #endregion
+
+
+        #region 方法
+
+
+
+        #endregion
+
+
+    }
+}
